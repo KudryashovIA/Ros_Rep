d05a12ee9647f6e1fd2f831538a08d37e94982ed
KudryashovIA <kia-snz@mail.ru>
2025-01-07 13:53:58 +0500

    2 - ROS2 BASIC CONCEPTS

---
+++ ros2_ws/build/build_test_pkg/colcon_command_prefix_setup_py.sh
@@ -0,0 +1,2 @@
+# generated from colcon_core/shell/template/command_prefix.sh.em
+. "/home/ivan/work/Ros_Rep/ros2_ws/install/build_test_pkg/share/build_test_pkg/package.sh"
---
+++ ros2_ws/install/my_package/share/my_package/launch/my_package_launch_file.launch.py
@@ -0,0 +1,1 @@
+/home/ivan/work/Ros_Rep/ros2_ws/build/my_package/launch/my_package_launch_file.launch.py
